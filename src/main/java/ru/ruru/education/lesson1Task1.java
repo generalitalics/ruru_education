d79ce2cea08c9f0035496284c8d6ab7aa6cfe434
generalitalics <generalitalics@mail.ru>
2017-10-20 11:13:22 +0300

    lesson2Task2

--- src/main/java/ru/ruru/education/lesson1Task1.java
+++ src/main/java/ru/ruru/education/lesson1Task1.java
@@ -1,14 +1,12 @@
 package ru.ruru.education;
 
-import sun.misc.FloatingDecimal;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 
 
-public class Lesson1 {
+public class lesson1Task1 {
     public static void vacancyGeneration(String s, int i, byte b, short sh, long l, boolean bl, double d, float f) {
         System.out.println("\nСрочно! Требуется адекватный человек для работы с системой будующего СУН2.0\n" +
                 "Расширяем группу тестирования в в компании " + s + " в Москве.\n" +
---
+++ src/main/java/ru/ruru/education/lesson2Task2.java
@@ -0,0 +1,24 @@
+package ru.ruru.education;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class lesson2Task2 {
+    public static int minTwo(int a, int b) {
+        if (a <= b) {
+            return a;
+        }
+        return b;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Поиск минимального числа из 2х");
+        System.out.print("введите 1-е число: ");
+        int x = Integer.parseInt(bufferedReader.readLine());
+        System.out.print("введите 2-е число: ");
+        int y = Integer.parseInt(bufferedReader.readLine());
+        System.out.println("Минимальное из 2х чисел: " + minTwo(x,y));
+    }
+}
+
