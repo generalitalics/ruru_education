199d14289bd3c2c4624978befe80a8b161712b60
generalitalics <generalitalics@mail.ru>
2017-11-08 15:51:50 +0300

    hamcrest добавил

--- src/test/java/ru/ruru/education/TestLesson2Task2.java
+++ src/test/java/ru/ruru/education/TestLesson2Task2.java
@@ -1,7 +1,8 @@
 package ru.ruru.education;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import static ru.ruru.education.Lesson2Task2.*;
 
@@ -9,16 +10,13 @@ public class TestLesson2Task2 {
 
     @Test()
     public void Test01() {
-        Assert.assertEquals(minTwo(1,2),1);
+    assertThat("Проверка minTwo()", minTwo(1,2), equalTo(1));
+        //Assert.assertEquals(minTwo(1,2),1);
     }
 
     @Test()
     public void Test02() {
-        Assert.assertEquals(minTwo(2147483647,-2147483647),-2147483647);
-    }
-
-    @Test()
-    public void Test04() {
-        Assert.assertEquals(minTwo(2,2147483647),-23);
+        //Assert.assertEquals(minTwo(2147483647,-2147483647),-2147483647);
+        assertThat("Проверяем JV_ERROR", minTwo(2147483647,-2147483647), equalTo(-2147483647));
     }
 }
