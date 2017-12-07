a5d66e562a4b56d9dc4883701b936eb32b8c8e40
generalitalics <generalitalics@mail.ru>
2017-12-07 18:51:22 +0300

    Доделал Lesson2

--- src/main/java/ru/ruru/education/Man.java
+++ src/main/java/ru/ruru/education/Man.java
@@ -2,17 +2,27 @@ package ru.ruru.education;
 
 class Man extends Human {
 
+    private Cat cat;
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
     Man(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+
+
     public static void main(String[] args) {
         Man man = new Man("Дэн", 30);
         Cat cat = new Cat("Мурка",11);
+
         cat.glasses = 5;
         System.out.println("Кошка по кличке " + cat.name + " разбила уже " + cat.glasses + " стаканов. " + man.name + " недоволен.");
         System.out.println(man.name + "у уже " + man.age + " лет. И он все еще живет с " + cat.age + "-летней кошкой");
-
+        man.setCat(cat);
+        man.cat.voice();
     }
 }
--- src/main/java/ru/ruru/education/Woman.java
+++ src/main/java/ru/ruru/education/Woman.java
@@ -2,10 +2,23 @@ package ru.ruru.education;
 
 class Woman extends Human {
 
+    private Dog dog;
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
     public Woman(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
+
+    public static void main(String[] args) {
+        Woman woman = new Woman("Lara",33);
+        Dog dog = new Dog("Laika",7);
+        woman.setDog(dog);
+        woman.dog.voice();
+    }
 
 }
